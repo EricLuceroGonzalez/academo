9eaa7e074dfc313731af16bd16646e1aaed8fe6d
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-07-31 18:07:54 -0500

    aa tes

--- routes/routes-control.js
+++ routes/routes-control.js
@@ -106,7 +106,7 @@ getPagueloFacil = async (req, res, next) => {
       `https://sandbox.paguelofacil.com/LinkDeamon.cfm?CCLW=9658182B95FC7E8FE5C5386BCD5E9BCCE2FABED4A71ED5536C4061BEB45AA2F67158527FE42CF10746B6758380D79B95B66FCF809474D8BC7D4D4C6B6B940689&CMTN=${req.body.amount}&RETURN_URL=${returnURl}&CDSC=Boleto%20para%20el%20show%3A%20${req.body.item_name}%7C%7C%20Ticket%20No%3A%20${req.body.order_key}`
     );
     // NodeMail Send:
-    transporter.sendMail(
+    await transporter.sendMail(
       {
         from: process.env.mailUser, // sender address
         to: "ericlucero501@gmail.com", // list of receivers
